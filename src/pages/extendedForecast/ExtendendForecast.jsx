263c89a3497babdd58bdc50f08606fae51d8cc5c
Leonardo Gabriel Borsetta <127697240+LeonardoBorsetta@users.noreply.github.com>
2023-07-04 13:24:56 -0500

    2.0

--- src/pages/extendedForecast/ExtendendForecast.jsx
+++ src/pages/extendedForecast/ExtendendForecast.jsx
@@ -1,9 +1,9 @@
-<<<<<<< HEAD
 import React, { useContext } from 'react'
 import {Link} from 'wouter'
 import Header from '../../components/header/Header'
 import WeatherContext from '../../context/weatherContext'
 import SingleDayForecast from '../../components/singleDayForecastMini/SingleDayForecastMini'
+import './extendendForecast.css'
 
 function ExtendendForecast() {
 
@@ -20,27 +20,4 @@ function ExtendendForecast() {
   )
 }
 
-=======
-import React, { useContext } from 'react'
-import {Link} from 'wouter'
-import Header from '../../components/header/Header'
-import WeatherContext from '../../context/weatherContext'
-import SingleDayForecast from '../../components/singleDayForecastMini/SingleDayForecastMini'
-
-function ExtendendForecast() {
-
-  const {weather} = useContext(WeatherContext)
-
-  return (
-    <div className="body-container">
-      <Header headerInfo = {weather.location}/>
-      <div className="forecast-forecastExtended-container">
-        <Link href='/tomorrow'><a><SingleDayForecast forecastData={weather.forecast.forecastday[1]} day={"Mañana"}/></a></Link>
-        <Link href='2dayNext'><a><SingleDayForecast forecastData={weather.forecast.forecastday[2]} day={"Mañana"}/></a></Link>
-      </div>
-    </div>
-  )
-}
-
->>>>>>> cdf4852f624bde8d165d00c36a1e9d078a35de68
 export default ExtendendForecast
--- src/pages/pasadoMañana/PasadoMañana.jsx
+++ src/pages/pasadoMañana/PasadoMañana.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useContext } from 'react'
 import WeatherContext from '../../context/weatherContext'
 import Header from '../../components/header/Header'
@@ -16,37 +15,10 @@ function PasadoMañana() {
     <div className="body-container">
       <h3>Clima para Pasado Mañana</h3>
       <Link href='/'><button className='searcher-button arrow-button'><img src={arrowRight} alt="Atras" className='arrow'/></button></Link>
-      <Link href='/each-hour-2day'><button className='searcher-button each-hour'><img src={clock} alt="" className='arrow'/>Por Hora</button></Link>
+      <Link href='/each-hour-2day'><button className='searcher-button each-hour'><img src={clock} alt="" className='arrow'/></button></Link>
       <Header headerInfo = {weather.location}/>
       <MainMañana weatherData={weather.forecast.forecastday[2].day}/>
     </div>
   )
 }
-
-=======
-import React, { useContext } from 'react'
-import WeatherContext from '../../context/weatherContext'
-import Header from '../../components/header/Header'
-import MainMañana from '../../components/mainMañana/MainMañana'
-import { Link } from 'wouter'
-import arrowRight from '../../assets/arrow-right-solid.svg'
-import clock from '../../assets/clock-solid.svg'
-
-function PasadoMañana() {
-
-  const {weather} = useContext(WeatherContext)
-  console.log(weather.forecast.forecastday[1].day)
-
-  return (
-    <div className="body-container">
-      <h3>Clima para Pasado Mañana</h3>
-      <Link href='/'><button className='searcher-button arrow-button'><img src={arrowRight} alt="Atras" className='arrow'/></button></Link>
-      <Link href='/each-hour-2day'><button className='searcher-button each-hour'><img src={clock} alt="" className='arrow'/>Por Hora</button></Link>
-      <Header headerInfo = {weather.location}/>
-      <MainMañana weatherData={weather.forecast.forecastday[2].day}/>
-    </div>
-  )
-}
-
->>>>>>> cdf4852f624bde8d165d00c36a1e9d078a35de68
 export default PasadoMañana
